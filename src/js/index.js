dec8cd79cf9c3f196fc1466997f022557ce31376
Todd Sayre <sayre_ts@mercer.edu>
2018-04-19 08:42:41 -0400

    Simplification
    I'm just styling an introductory section. JavaScript rearranges the contents of the page.

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,39 @@
  * @author Todd Sayre
  */
 
-document.addEventListener('DOMContentLoaded', function() {
+import prependChild from 'prepend-child';
+import domLoaded from 'dom-loaded';
+
+/**
+ *
+ */
+const onLoad = function () {
+    moveAad();
+    copyAadssoLinks();
+    removeAadssoLoginFormText();
+};
+
+/**
+ * Move the `.aad` block out from the `#loginform` form to be the first child of the `body`
+ */
+const moveAad = function () {
+    let aad = document.querySelector('.aad');
+    let body = document.querySelector('body');
+    prependChild(body, aad);
+};
+
+/**
+ * Remove the AADSSO-plugin-added content
+ */
+const removeAadssoLoginFormText = function () {
+    let aadssoLoginFormText = document.querySelector('.aadsso-login-form-text');
+    aadssoLoginFormText.remove();
+};
+
+/**
+ * Add AADSSO-plugin-generated URLs to this-plugin-generated links
+ */
+const copyAadssoLinks = function () {
     let aLogin = document.querySelector('.aadsso-login-form-text a[href*="authorize"]');
     let aLogout = document.querySelector('.aadsso-login-form-text a[href*="logout"]');
     let urlLogin = aLogin.getAttribute('href');
@@ -11,4 +43,6 @@ document.addEventListener('DOMContentLoaded', function() {
     let aMULogout = document.querySelector('.aad__cta--logout');
     aMULogin.setAttribute('href', urlLogin);
     aMULogout.setAttribute('href', urlLogout);
-});
+};
+
+domLoaded.then(onLoad);
