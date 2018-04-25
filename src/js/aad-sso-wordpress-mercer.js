bf04d418a9746272619820da2af47624f7f6bbf1
Todd Sayre <captain.friendly@gmail.com>
2018-04-25 16:26:01 -0400

    Cleanup if it's not needed
    Closes #4 with some pure JavaScript. That seems the cleanest solution.

--- src/js/aad-sso-wordpress-mercer.js
+++ src/js/aad-sso-wordpress-mercer.js
@@ -11,33 +11,26 @@ export default class AadSsoWordPressMercer {
     /**
      * construct it after DOMContentLoaded because this rearranges the markup
      */
-    constructor () {
+    constructor() {
+
+        if (! AadSsoWordPressMercer.isAadSsoWordPressPresent()) {
+            // console.log('abandoning');
+            AadSsoWordPressMercer.removeAad();
+            return;
+        }
+
+        // console.log('proceeding');
+        AadSsoWordPressMercer.helpWith100vhCss();
         AadSsoWordPressMercer.prependAadToBody();
         AadSsoWordPressMercer.copyAadSsoLinks();
         AadSsoWordPressMercer.removeAadSsoLoginFormText();
+
     }
-
-    /**
-     * Move the `.aad` block out from the `#loginform` form to be the first child of the `body`
-     */
-    static prependAadToBody () {
-        let aad = document.querySelector('.aad');
-        let body = document.querySelector('body');
-        prependChild(body, aad);
-    };
-
-    /**
-     * Remove the AADSSO-plugin-added content
-     */
-    static removeAadSsoLoginFormText () {
-        let aadssoLoginFormText = document.querySelector('.aadsso-login-form-text');
-        aadssoLoginFormText.remove();
-    };
 
     /**
      * Add AADSSO-plugin-generated URLs to this-plugin-generated links
      */
-    static copyAadSsoLinks () {
+    static copyAadSsoLinks() {
         let aLogin = document.querySelector('.aadsso-login-form-text a[href*="authorize"]');
         let aLogout = document.querySelector('.aadsso-login-form-text a[href*="logout"]');
         let urlLogin = aLogin.getAttribute('href');
@@ -47,4 +40,63 @@ export default class AadSsoWordPressMercer {
         aMULogin.setAttribute('href', urlLogin);
         aMULogout.setAttribute('href', urlLogout);
     };
+
+    /**
+     * Make mobile browsers behave better with 100vh CSS
+     *
+     * https://brentstromberg.com/jank-free-vh-sections/
+     *
+     * Adapted to not use jQuery
+     */
+    static helpWith100vhCss() {
+
+        // $heightOnLoad = $('.vc_row.vc_row-o-full-height.welcome-message-wrapper').height();
+        // console.log($heightOnLoad);
+        // $(window).resize(function() {
+        //     $('body.mobile .vc_row.vc_row-o-full-height.welcome-message-wrapper').css({
+        //         'min-height':$heightOnLoad
+        //     });
+        // });
+
+        let aad = document.querySelector('.aad');
+        let height = `${aad.offsetHeight}px`;
+        console.log(height);
+        aad.style.minHeight = height;
+    }
+
+    /**
+     *
+     */
+    static isAadSsoWordPressPresent() {
+        let aadssoLoginFormText = document.querySelector('.aadsso-login-form-text');
+        // console.log(aadssoLoginFormText);
+        // console.log(null !== aadssoLoginFormText);
+        return null !== aadssoLoginFormText;
+    }
+
+    /**
+     * Move the `.aad` block out from the `#loginform` form to be the first child of the `body`
+     */
+    static prependAadToBody() {
+        let aad = document.querySelector('.aad');
+        let body = document.querySelector('body');
+        prependChild(body, aad);
+    };
+
+    /**
+     * Remove the AAD block
+     */
+    static removeAad() {
+        let aad = document.querySelector('.aad');
+        aad.remove();
+    }
+
+    /**
+     * Remove the AADSSO-plugin-added content
+     */
+    static removeAadSsoLoginFormText() {
+        let aadssoLoginFormText = document.querySelector('.aadsso-login-form-text');
+        aadssoLoginFormText.remove();
+    };
+
 }
--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,9 @@
 import domLoaded from 'dom-loaded';
 import AadSsoWordPressMercer from './aad-sso-wordpress-mercer';
 
+// This isn't going to work as an `import`
+require('viewport-units-buggyfill').init();
+
 const onLoad = function () {
     new AadSsoWordPressMercer();
 };
