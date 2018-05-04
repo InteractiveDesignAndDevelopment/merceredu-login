c2cf732509e651896bc8c2b04de7baf9f0236959
Todd Sayre <captain.friendly@gmail.com>
2018-05-04 13:00:05 -0400

    Webpack works
    Webpack is working fine and the build goes faster than ever before.

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,17 @@ const path = require('path');
 
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const extractSass = new ExtractTextPlugin({
-    filename: '[name].css'
+    filename: 'aad-sso-wordpress-mercer.css'
 });
 
-const optimizeCssAssets= new OptimizeCssAssetsPlugin({
-    assetNameRegExp: /\.optimize\.css$/g,
-    cssProcessor: require('cssnano'),
-    cssProcessorOptions: { discardComments: { removeAll: true } },
-    canPrint: true
-});
+const optimizeCssAssets = new OptimizeCssAssetsPlugin();
+
+const copyFiles = new CopyWebpackPlugin([
+    'src/php/aad-sso-wordpress-mercer.php'
+]);
 
 module.exports = {
     mode: 'development',
@@ -24,21 +24,15 @@ module.exports = {
     module: {
         rules: [{
             test: /\.scss$/,
-            // use: [{
-            //     loader: 'style-loader' // Creates style nodes from JS strings
-            // }, {
-            //     loader: 'css-loader' // Translates CSS into CommonJS
-            // },{
-            //     loader: 'resolve-url-loader' // Must precede sass-loader
-            // }, {
-            //     loader: 'sass-loader' // Compiles Sass to CSS
-            // }]
-            // use: ['style-loader', 'css-loader', 'resolve-url-loader', 'sass-loader']
             use: extractSass.extract(['css-loader', 'resolve-url-loader', 'sass-loader'])
+        }, {
+            test: /\.(jpe?g|png|ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+            use: 'base64-inline-loader?limit=1000&name=[name].[ext]'
         }]
     },
     plugins: [
         extractSass,
-        optimizeCssAssets
+        optimizeCssAssets,
+        copyFiles
     ]
 };
