250e37d74beb6a9cdc5fd3ac152835379b31ea85
Todd Sayre <captain.friendly@gmail.com>
2018-05-04 14:08:17 -0400

    Production-read Webacking
    Webpack is in production mode now. I think I understand more-than-50% of what it's doing now!

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 const extractSass = new ExtractTextPlugin({
     filename: 'aad-sso-wordpress-mercer.css'
@@ -10,12 +11,16 @@ const extractSass = new ExtractTextPlugin({
 
 const optimizeCssAssets = new OptimizeCssAssetsPlugin();
 
-const copyFiles = new CopyWebpackPlugin([
+const copy = new CopyWebpackPlugin([
     'src/php/aad-sso-wordpress-mercer.php'
 ]);
 
+const clean = new CleanWebpackPlugin(
+    ['dist']
+);
+
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: './src/js/index.js',
     output: {
         filename: 'aad-sso-wordpress-mercer.js',
@@ -28,11 +33,21 @@ module.exports = {
         }, {
             test: /\.(jpe?g|png|ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
             use: 'base64-inline-loader?limit=1000&name=[name].[ext]'
+        },{
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
         }]
     },
     plugins: [
         extractSass,
         optimizeCssAssets,
-        copyFiles
+        copy,
+        clean
     ]
 };
